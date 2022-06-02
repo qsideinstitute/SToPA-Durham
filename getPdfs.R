3052ec8bc92cb22c7566287a6ea07ce45af778a6
chadtopaz <chad.topaz@gmail.com>
2022-06-02 10:23:57 -0400

    Got rid of unneeded archive file, small updates to main code.

--- getPdfs.R
+++ getPdfs.R
@@ -2,7 +2,6 @@
 library(gitcreds)
 library(tidyverse)
 library(RSelenium)
-library(pbmcapply)
 
 # Set up a download path
 # Must be consistent with whatever is in browserProfile
@@ -10,7 +9,7 @@ downloadPath <- "data/"
 browserProfile <- getFirefoxProfile("browserProfile/")
 
 # Start Selenium
-rD <- rsDriver(browser="firefox", port=4545L, verbose=F, extraCapabilities = browserProfile)
+rD <- rsDriver(browser="firefox", port=4546L, verbose=F, extraCapabilities = browserProfile)
 remDr <- rD[["client"]]
 
 # Set search page URL
@@ -52,9 +51,8 @@ startPicker$clearElement()
 startPicker$sendKeysToElement(list(startdate))
 endPicker$clearElement()
 endPicker$sendKeysToElement(list(enddate))
-remDr$click(buttonId = "search-button")
-searchButton <- remDr$findElement(using = "id", value = "search-button")
 Sys.sleep(1)
+searchButton <- remDr$findElement(using = "id", value = "search-button")
 searchButton$clickElement()
 
 # Find "Load More" button and continue to click until no more can be loaded
@@ -73,8 +71,9 @@ while (loadFlag == TRUE) {
 # Get button for each result
 results <- remDr$findElements(using = "css selector", value = "div.p2c-eventSearch-result > md-card:nth-child(1) > md-card-content:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > i:nth-child(1)")
 
+
 # Get files
-for (i in 1:length(results)) {
+for (i in 1:10) {
   if (i %% 10 == 0) print(i)
   results[[i]]$clickElement()
   Sys.sleep(1.5)
@@ -91,11 +90,7 @@ origFiles <- paste0(downloadPath,"pdf(",idx,")")
 rename <- function(i){
   tryCatch(out <- file.rename(origFiles[i],filenames[i]))
 }
-pbmclapply(idx, rename)
-
-
-out <- file.rename(paste0(downloadPath,"pdf"),filenames[i])
-
+lapply(idx, rename)
 
 # Shut down Selenium
 rD[["server"]]$stop()
